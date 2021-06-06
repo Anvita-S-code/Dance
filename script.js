cae6b9d2135d1628693692e58678363a9215c46f
Anvita Sindhur <anvitasindhur@gmail.com>
2021-06-06 17:39:45 +0530

    Updated
    updated

---
+++ script.js
@@ -0,0 +1,25 @@
+function myFunction (flower){
+
+ 
+
+    if(flower == 'intro'){
+     var elemt = document.getElementById("intro");
+    elemt.scrollIntoView();
+         
+    }else if (flower == 'mudras'){
+    var elemt = document.getElementById("mudras");
+    elemt.scrollIntoView();
+
+    }else if (flower == 'adavus') {
+    var elemt = document.getElementById("adavus");
+    elemt.scrollIntoView();
+    }else if (flower == 'costume') {
+        var elemt = document.getElementById("costume");
+        elemt.scrollIntoView();
+        }
+    
+else {
+    var elemt = document.getElementById("makeup");
+    elemt.scrollIntoView();
+    
+    }}
